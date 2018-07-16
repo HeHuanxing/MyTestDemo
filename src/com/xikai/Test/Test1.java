ad2fb70fa4d3ad16cf6092a9e6b7a218d54d0ca3
master <abc@com>
2018-07-16 15:37:22 +0800

    这时第二次提交项目

--- src/com/xikai/Test/Test1.java
+++ src/com/xikai/Test/Test1.java
@@ -3,5 +3,6 @@ package com.xikai.Test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是一个测试项目");
+        System.out.println("现在是第二次修改项目并提交");
     }
 }
