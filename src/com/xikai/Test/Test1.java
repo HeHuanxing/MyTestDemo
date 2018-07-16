4cc9c504a52d221bd9ddf9797c70b507c5700654
master <abc@com>
2018-07-16 15:41:12 +0800

    这时第三次提交项目

--- src/com/xikai/Test/Test1.java
+++ src/com/xikai/Test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是一个测试项目");
         System.out.println("现在是第二次修改项目并提交");
+        System.out.println("现在是第三次修改项目并提交");
     }
 }
